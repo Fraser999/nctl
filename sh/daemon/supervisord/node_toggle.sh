e427093efe0f6df6eb23e1ebd087228ca77c29ff
Mark A. Greenslade <mark@casperlabs.io>
2020-09-28 11:26:22 +0200

    1. New command to toggle node state.  2. Document update.

--- sh/daemon/supervisord/node_toggle.sh
+++ sh/daemon/supervisord/node_toggle.sh
@@ -18,6 +18,12 @@ echo $2
 node_status=$(supervisorctl -c "$(get_path_net_supervisord_cfg $1)" status "$(get_node_process_name $1 $2)")
 
 case "$node_status" in 
+  *EXITED*)
+    source $NCTL/sh/daemon/supervisord/node_start.sh $1 $2
+    ;;
+  *FATAL*)
+    source $NCTL/sh/daemon/supervisord/node_start.sh $1 $2
+    ;;
   *RUNNING*)
     source $NCTL/sh/daemon/supervisord/node_stop.sh $1 $2
     ;;
@@ -30,19 +36,3 @@ esac
 # TODO: move Display nodeset state to containing.
 sleep 2.0
 source $NCTL/sh/daemon/supervisord/node_status.sh $1
-
-
-
-# Ensure daemon is up.
-# source $NCTL/sh/daemon/supervisord/daemon_start.sh $1
-
-# Stop nodeset.
-# if [ $2 = "all" ]; then
-#     supervisorctl -c "$(get_path_net_supervisord_cfg $1)" stop all  > /dev/null 2>&1
-# else
-#     supervisorctl -c "$(get_path_net_supervisord_cfg $1)" stop "$(get_node_process_name $1 $2)"  > /dev/null 2>&1
-# fi
-
-# Display nodeset state.
-# sleep 2.0
-# source $NCTL/sh/daemon/supervisord/node_status.sh $1
